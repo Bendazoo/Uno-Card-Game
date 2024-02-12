e6700d0b8ced87452562f170935f5fe4c3b0777c
bendazoo <abendezu385@gmail.com>
2024-02-12 16:39:12 -0500

    Created GameLoop Class and added InputMisMatchException

---
+++ Uno-Card-Game/src/GameLoop.java
@@ -0,0 +1,9 @@
+public class GameLoop {
+
+    void GameLoop(){
+
+
+
+    }
+
+}
--- Uno-Card-Game/src/Main.java
+++ Uno-Card-Game/src/Main.java
@@ -1,8 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userOption;
+        boolean error;
 
-        }
+        System.out.println("    Welcome to Uno!");
+        do {
+            error = false;
+            try {
+
+                System.out.println("   Select an option: ");
+                System.out.println("=======================");
+                System.out.println("\tRules: 1 ");
+                System.out.println("\tPlay: 2 ");
+                System.out.println("\tExit: 3 ");
+                userOption = scanner.nextInt();
+
+            } catch (InputMismatchException e) {
+
+                System.out.println("Input a correct option.");
+                System.out.println("=======================");
+                error = true;
+                scanner.nextLine();
+
+            }
+        } while(error);
+
+
+
     }
 }
